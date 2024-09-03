1ffeafd26761e8c581aacd8ca2a77aa4d0ef362d
Amisha20223 <amisha.2125ec1132@kiet.edu>
2024-09-03 12:09:31 +0530

    Float

---
+++ printing.java
@@ -0,0 +1,11 @@
+
+public class printing {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+         float a =  234.169f;
+        System.out.printf("Formatted number is %.1f",a);
+    }
+    
+}
