e6d136cb87a00816cb99697195fc213df2a7df6c
Amisha Singh <120022430+Amisha20223@users.noreply.github.com>
2024-10-22 22:52:47 +0530

    Time: 1 ms (84.43%) | Memory: 41.5 MB (74.56%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -4,17 +4,21 @@ class Solution {
         int i = 0, j = 0;
 
         
-        while (i < word1.length() || j < word2.length()) {
-            if (i < word1.length()) {
-                sb.append(word1.charAt(i));
-                i++;
-            }
-            if (j < word2.length()) {
-                sb.append(word2.charAt(j)); 
-                j++;
-            }
+        while (i < word1.length() && j < word2.length()) {
+            sb.append(word1.charAt(i++));
+            sb.append(word2.charAt(j++));
         }
 
-        return sb.toString(); 
+       
+        while (i < word1.length()) {
+            sb.append(word1.charAt(i++));
+        }
+
+    
+        while (j < word2.length()) {
+            sb.append(word2.charAt(j++));
+        }
+
+        return sb.toString();
     }
 }
