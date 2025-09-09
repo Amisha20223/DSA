0b492240ef12fcf5be3c438509a6973d7068f225
Amisha Singh <120022430+Amisha20223@users.noreply.github.com>
2025-09-09 23:15:56 +0530

    Time: 10 ms (43.64%) | Memory: 41.3 MB (19.09%) - LeetSync

---
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        int MOD = 1_000_000_007;
+        long[] dp = new long[n + 1];
+        dp[1] = 1; 
+
+        for (int day = 1; day < n; day++) {
+            long sharers = dp[day];
+            if (sharers == 0) continue;
+            
+            for (int j = day + delay; j < day + forget && j <= n; j++) {
+                dp[j] = (dp[j] + sharers) % MOD;
+            }
+        }
+
+        long ans = 0;
+        
+        for (int i = n - forget + 1; i <= n; i++) {
+            if (i > 0) ans = (ans + dp[i]) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
+
