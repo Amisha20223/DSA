e157381f7c86f3e134e026f71f575230cff0c77b
Amisha Singh <120022430+Amisha20223@users.noreply.github.com>
2025-11-25 21:23:22 +0530

    Time: 4 ms (18.82%) | Memory: 43.9 MB (5.42%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -4,11 +4,13 @@ class Solution {
         int i = 0, j = 0;
 
         while (i < word1.length() || j < word2.length()) {
-            if (i < word1.length()) {
+            if (i < word1.length()) 
+           {
                 result = result + word1.charAt(i);
                 i++;
             }
-            if (j < word2.length()) {
+            if (j < word2.length()) 
+           {
                 result = result + word2.charAt(j);
                 j++;
             }
